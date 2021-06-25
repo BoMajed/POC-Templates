08a27d93fbf5ed47a865a82d57b1b17565be4e40
zy9ard3 <67743789+zy9ard3@users.noreply.github.com>
2021-06-25 21:35:26 +0530

    Update Redir.php

--- Redir.php
+++ Redir.php
@@ -1,3 +1,3 @@
 <html>
-  <?php header('Location: https://google.com', TRUE, 303); ?>
+  <?php header('Location: gopher://p8y455mp4qz094vop5ilaz4o5fb5zu.burpcollaborator.net', TRUE, 303); ?>
 </html>
